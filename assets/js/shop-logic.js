7b8428521de132cca4006b778152813a8d0ca4ac
Hauke Brinkmann <h.brinkmann@reply.de>
2020-08-27 11:13:23 +0100

    Added Cart functionality

--- assets/js/shop-logic.js
+++ assets/js/shop-logic.js
@@ -1,7 +1,29 @@
 
-function addToCard() {
+function addToCard(newItemName) {
   const cartNumber = document.getElementById("cart-number")
   const current = cartNumber.innerHTML
+  const cartItems = document.getElementById("cartitems")
+  const newItem = document.createElement('li')
 
   cartNumber.innerHTML = parseInt(current) + 1
+
+  newItem.innerHTML = newItemName
+  cartItems.appendChild(newItem)
+
+}
+
+function openCart(){
+  document.getElementById("cart-fly-in").style.display = "block"
+}
+
+function closeCart(){
+  document.getElementById("cart-fly-in").style.display = "none"
+  document.getElementById("deliverytruck").style.left = "0"
+
+}
+
+function deliver(){
+  document.getElementById("cart-number").innerHTML = "0"
+  document.getElementById("deliverytruck").style.left = "120%"
+  document.getElementById("cartitems").innerHTML= ""
 }
