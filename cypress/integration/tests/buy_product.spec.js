dcb2fec609f4d77d74b313d4aff06bd0c5f70852
Hauke Brinkmann <h.brinkmann@reply.de>
2020-08-27 15:29:19 +0100

    Tests

---
+++ cypress/integration/tests/buy_product.spec.js
@@ -0,0 +1,30 @@
+
+  describe('Buying a Product', () => {
+
+    beforeEach(() => {
+      cy.visit('/')
+    })
+
+    it('Happy path', () => {
+      cy.get('[data-cy="cart-amount"]').contains('0')
+      cy.get('[data-cy="topseller-item"]').first().find('button').click()
+      cy.get('[data-cy="cart-amount"]').contains('1')
+
+      cy.get('[data-cy="cart"]').click()
+      cy.get('[data-cy="cart-fly-in"]').should('be.visible')
+      cy.get('[data-cy="cart-fly-in-items"]').find('li').should('have.length', 1)
+
+      cy.get('[data-cy="cart-fly-in"]').find('button').click()
+
+      cy.get('[data-cy="cart-fly-in-items"]').find('li').should('have.length', 0)
+      cy.get('#deliverytruck').should('not.be.visible')
+    })
+
+    it('Cart functionality', ()=>{
+      cy.get('[data-cy="cart"]').click()
+      cy.get('[data-cy="cart-fly-in"]').should('be.visible')
+      cy.get('[data-cy="cart-fly-in-close"]').click()
+      cy.get('[data-cy="cart-fly-in"]').should('not.be.visible')
+    })
+
+  })
---
+++ cypress/integration/tests/landingpage.spec.js
@@ -0,0 +1,39 @@
+
+  describe('Landingpage Content', () => {
+
+    before(() => {
+      cy.visit('/')
+    })
+
+    it('Header and footer', () => {
+      cy.get('[data-cy="header"]').should('be.visible')
+      cy.get('[data-cy="logo"]').should('be.visible')
+      cy.get('[data-cy="logo-claim"]').should('be.visible')
+      cy.get('[data-cy="cart"]').should('be.visible')
+      cy.get('[data-cy="cart-amount"]').should('be.visible')
+      cy.get('[data-cy="footer-link"]').should('be.visible')
+    })
+
+    it('Important content should be visible', () => {
+      cy.get('[data-cy="topseller-item"]')
+        .should('be.visible')
+        .should('have.length', 3)
+
+      cy.get('[data-cy="topseller-item"]').first().find('img').should('be.visible')
+      cy.get('[data-cy="topseller-item"]').first().find('span').should('be.visible')
+      cy.get('[data-cy="topseller-item"]').first().find('button').should('be.visible')
+
+      cy.get('[data-cy="all-products-link"]').should('be.visible')
+      cy.get('[data-cy="ceo-text"]').should('be.visible')
+    })
+
+    it('Links are working', ()=>{
+      cy.get('[data-cy="all-products-link"]').click()
+      cy.url().should('contain', 'products')
+
+      cy.visit('/')
+      cy.get('[data-cy="footer-link"]').click()
+      cy.url().should('contain', 'imprint')
+    })
+
+  })
