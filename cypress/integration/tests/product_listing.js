27dcba2afa4902d556ac470e4fdab0a48957580c
Hauke Brinkmann <h.brinkmann@reply.de>
2020-08-28 11:20:40 +0100

    Change API Call to XHR

--- cypress/integration/tests/product_listing.js
+++ cypress/integration/tests/product_listing.js
@@ -6,6 +6,17 @@
     })
 
     it('Loading products', ()=>{
+      cy.server()
+      cy.route('GET', '**/albums').as('albums');
+
+      cy.get('[data-cy="product-list-loading"]').should('be.visible')
+      cy.wait('@albums');
+
+      cy.get('[data-cy="product-list-loading"]').should('not.be.visible')
+      cy.get('[data-cy="product-list-item"]').should('be.visible')
+    })
+
+    it('Loading products without XHR wait', ()=>{
       cy.get('[data-cy="product-list-loading"]').should('be.visible')
 
       cy.get('[data-cy="product-list-loading"]').should('not.be.visible')
